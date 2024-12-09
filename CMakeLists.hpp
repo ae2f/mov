feb93e6aeaed42f108c56630ece74c82b63b24fc
ae2f <mincraft20046@gmail.com>
2024-12-09 22:16:09 +0900

    Rel-v0.3.1

---
+++ CMakeLists.hpp
@@ -0,0 +1,13 @@
+#define ON 1
+#define OFF 0
+
+namespace ____ae2f_Core_Def_CMAKE {
+    
+    /// @brief 
+    /// Affects the integer default types. \n
+    /// @see ae2f_MovScalar_t
+    /// @see ae2f_MovVec_t
+    /// Default value is OFF.
+    /// @see ae2f/mov/Int.h
+    constexpr bool ae2f_mov_IS_64 = OFF;
+}
